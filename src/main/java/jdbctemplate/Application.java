664cc419d15eb368c82388820da9f1e6732dc958
Frederic Moal <frederic.moal@univ-orleans.fr>
2017-11-15 10:13:37 +0100

    ajout chapitre 2 v1.0 snapshot

--- src/main/java/jdbctemplate/Application.java
+++ src/main/java/jdbctemplate/Application.java
@@ -31,27 +31,32 @@ public class Application implements CommandLineRunner {
         log.info("Creating tables");
 
         jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE customers(" +
-                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+        jdbcTemplate.execute("CREATE TABLE customers("
+          + "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
         // end::createTable[]
 
         // tag::insertTable[]
         // Split up the array of whole names into an array of first/last names
-        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+        List<Object[]> splitUpNames =
+            Arrays
+                .asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+                .stream()
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
-
         // Use a Java 8 stream to print out each tuple of the list
-        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-
+        splitUpNames.forEach(
+                name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1]))
+        );
         // Uses JdbcTemplate's batchUpdate operation to bulk load data
-        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO customers(first_name, last_name) VALUES (?,?)",
+                splitUpNames);
         // end::insertTable[]
 
         log.info("Querying for customer records where first_name = 'Josh':");
         // tag::selectTable[]
         jdbcTemplate.query(
-                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+            "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
                 (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
         ).forEach(customer -> log.info(customer.toString()));
         // end::selectTable[]
